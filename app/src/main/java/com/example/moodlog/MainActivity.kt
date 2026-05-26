6f63ca6b6cc448f712f284531233e7e22aa8c7e4
Guilherme <guilherme@Guilhermes-MacBook-Air.local>
2026-05-26 00:40:11 -0300

    Adiciona controle de estado e feedback visual na MainActivity

--- app/src/main/java/com/example/moodlog/MainActivity.kt
+++ app/src/main/java/com/example/moodlog/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.moodlog
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var quantidadeAberturas = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Recupera valor salvo ao girar a tela
+        quantidadeAberturas = savedInstanceState?.getInt("aberturas") ?: 0
+
+        quantidadeAberturas++
+
+        Toast.makeText(
+            this,
+            "MoodLog iniciado $quantidadeAberturas vez(es)",
+            Toast.LENGTH_SHORT
+        ).show()
+
+        supportActionBar?.title = "MoodLog - Diário de Humor"
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        // Salva quantidade de aberturas
+        outState.putInt("aberturas", quantidadeAberturas)
     }
 }
